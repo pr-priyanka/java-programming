22c9ae0aebb0411530711b549dab0da2fa7ae9cf
Priyanka Priya <109232090+pr-priyanka@users.noreply.github.com>
2023-01-21 21:02:45 +0530

    Create FibonacciNumber.java

---
+++ FibonacciNumber.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+	
+	public static boolean checkMember(int n){
+            int a = 0;
+            int b = 1;
+            if (n==a || n==b) 
+			return true;
+            int c = a+b;
+            while(c<=n)
+            {
+                if(c == n) 
+				return true;
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            return false;
+
+	}
+	
+
+}
