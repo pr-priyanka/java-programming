1fe56c67bcc3f4ad4dc42dca789424d8ea9e932b
Priyanka Priya <109232090+pr-priyanka@users.noreply.github.com>
2023-01-21 20:49:12 +0530

    Create PowerOfANumber.java

---
+++ PowerOfANumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+        int x, n;
+        x=s.nextInt();
+        n=s.nextInt();
+      double ans=Math.pow(x,n);
+        System.out.println((int)ans);
+        
+        
+        
+         
+    }
+}
