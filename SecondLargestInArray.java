c5501124e05e9013f6ae6ccf04bf13ee3047a0bc
Priyanka Priya <109232090+pr-priyanka@users.noreply.github.com>
2023-02-04 17:16:35 +0530

    Create SecondLargestInArray.java

---
+++ SecondLargestInArray.java
@@ -0,0 +1,24 @@
+public class Solution {  
+
+    public static int secondLargestElement(int[] arr) {
+    	 int len = arr.length;
+        int first;
+        int second;
+        
+        first = second = Integer.MIN_VALUE;
+        for (int i = 0; i<len; i++){
+            if (first<arr[i]){
+                first = arr[i];
+            }
+        }
+        
+        for (int j = 0; j<len; j++){
+            if (arr[j] != first){
+                second = Math.max(second, arr[j]);
+            }
+        }
+        
+        return second;
+    }
+
+}
