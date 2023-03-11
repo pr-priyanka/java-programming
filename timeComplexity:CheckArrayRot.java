71e51fe5651913ea9919c1b6d7649a825ca52d7d
Priyanka Priya <109232090+pr-priyanka@users.noreply.github.com>
2023-03-11 12:19:56 +0530

    Create timeComplexity:CheckArrayRot.java

---
+++ timeComplexity:CheckArrayRot.java
@@ -0,0 +1,16 @@
+int len = arr.length;
+        int min_element = Integer.MAX_VALUE;
+        int min_index = Integer.MAX_VALUE;
+        
+         if (len==0){
+             return 0;
+         }
+        
+        for(int i = 0; i<len; i++){
+            if(arr[i]<min_element){
+                min_element = arr[i];
+                min_index = i;
+            }
+        }
+        
+        return min_index;
