45a2764453e506094c9d25b2f3a5e1983c499312
Priyanka Priya <109232090+pr-priyanka@users.noreply.github.com>
2023-03-12 18:27:53 +0530

    Create recursion:MultiplicationOfTwoNo.java

---
+++ recursion:MultiplicationOfTwoNo.java
@@ -0,0 +1,20 @@
+
+public class solution {
+
+	public static int multiplyTwoIntegers(int m, int n){
+		// Write your code here
+		if(m==0 || n==0){
+			return 0;
+
+		}
+		int sum=0;
+		for(int i=1;i<=m-1;i++){
+			sum=sum+n;
+		}
+		int out=sum+n;
+		return ;
+		
+		
+	
+	}
+}
