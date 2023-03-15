9aea8d7b0192e0c0af5733c580bba577ca5cfa11
Priyanka Priya <109232090+pr-priyanka@users.noreply.github.com>
2023-03-15 10:59:20 +0530

    Create LinkedList:DeleteNode.java

---
+++ LinkedList:DeleteNode.java
@@ -0,0 +1,50 @@
+/****************************************************************
+
+	Following is the Node class already written for the Linked List
+
+	class LinkedListNode<T> {
+    	T data;
+    	LinkedListNode<T> next;
+    
+    	public LinkedListNode(T data) {
+        	this.data = data;
+    	}
+	}
+
+*****************************************************************/
+
+public class Solution {
+	public static LinkedListNode<Integer> deleteNode(LinkedListNode<Integer> head, int pos) {
+		// Write your code here.
+		  if (head ==null)
+        {
+            return head;
+        }
+            
+        else if(pos==0)
+        {
+            head=head.next;
+        }
+        else
+        {
+            LinkedListNode<Integer> n=head,delNode=null;
+            for (int i=0;i<pos-1 && n!=null;i++)
+            {
+                n=n.next;
+            }
+            if (n!=null && n.next!=null)
+            {
+                if (n.next!=null)
+                {
+                    n.next=n.next.next;
+                }
+                else
+                {
+                    n.next=null;
+                }
+                
+            }
+        }
+        return head;
+	}
+}
