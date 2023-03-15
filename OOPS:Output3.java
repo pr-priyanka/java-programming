6fb24178746669fdeb58e780108690eeee9ad938
Priyanka Priya <109232090+pr-priyanka@users.noreply.github.com>
2023-03-15 14:05:19 +0530

    Create OOPS:Output3.java

---
+++ OOPS:Output3.java
@@ -0,0 +1,21 @@
+class Vehicle{
+    void print(){
+    System.out.print("Vehicle class");
+    }
+}
+class Car extends Vehicle{
+    void print(){
+    System.out.print("Car class ");
+    }
+}
+
+public class Main {
+    public static void main(String[] args){   
+    Vehicle obj1 = new Car();
+    obj1.print();
+
+    Vehicle obj2 = new Vehicle();
+    obj2.print();  
+    }
+}
+//Car class Vehicle class
